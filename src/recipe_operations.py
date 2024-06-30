bbe91da691ba533a581b5af282e3574252a1ddaa
chung-v <vcdott@gmail.com>
2024-07-01 01:11:27 +1000

    added method and function to export recipe.

--- src/recipe_operations.py
+++ src/recipe_operations.py
@@ -27,6 +27,22 @@ class RecipeManager:
                 return
         print(f"Recipe not found.")
 
+    # Method to export recipe
+    def export_recipe(self, recipe_name, txt_file):
+        with open(txt_file, 'w') as file:
+            for recipe in self.recipes:
+                if recipe.name.lower() == recipe_name.lower():
+                    file.write(f"Recipe: {recipe.name}\n")
+                    file.write("Ingredients:\n")
+                    for ingredient in recipe.ingredients:
+                        file.write(f"- {ingredient}\n")
+                    file.write("Instructions:\n")
+                    for step, instruction in enumerate(recipe.instructions, 1):
+                        file.write(f"{step}. {instruction}\n")
+                    print(f"Recipe '{recipe_name}' exported to {txt_file}.")
+                    return
+            print(f"Recipe not found.")
+
 # Creating object instance for class RecipeManager
 manager = RecipeManager()
 
@@ -58,5 +74,14 @@ def display_recipe(manager):
     try:
         recipe_name = input("Select recipe: ")
         manager.display_recipe(recipe_name)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}.")
+
+# Function to export recipe
+def export_recipe(manager):
+    try:
+        recipe_name = input("Enter recipe name to export: ")
+        txt_file = input("Enter name for txt file (e.g. 'recipe.txt'): ")
+        manager.export_recipe(recipe_name, txt_file)
     except Exception as e:
         print(f"An unexpected error occurred: {e}.")
