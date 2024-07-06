d87d1e76d745431adc576fba5ca983bd7e4603ae
chung-v <vcdott@gmail.com>
2024-07-07 01:36:49 +1000

    changed text format and colour via pyfiglet and rich. added packages to requirements file.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
 from file_operations import load_recipe, save_recipe
 from recipe_operations import add_recipe, display_recipe, remove_recipe, export_recipe
 
+import pyfiglet
+from rich import print
+
 FILE_PATH = '../data/recipes.json'
 
 def main():
     manager = load_recipe(FILE_PATH)
 
     while True:
-            print("\nRecipe Manager:\n")
-            print("1. Add a recipe")
-            print("2. Select a recipe")
-            print("3. Remove a recipe")
-            print("4. Export a recipe")
-            print("5. Exit")
+            title = pyfiglet.figlet_format('RECIPE MANAGER', font='doom')
+            print(f'[cornflower_blue]{title}[/]')
+            print("[#8190BB]1.[/] Add a recipe")
+            print("[#8190BB]2.[/] Select a recipe")
+            print("[#8190BB]3.[/] Remove a recipe")
+            print("[#8190BB]4.[/] Export a recipe")
+            print("[#8190BB]5.[/] Exit")
 
             choice = input("\nChoose an option: ")
 
@@ -27,10 +31,10 @@ def main():
             elif choice == '4':
                 export_recipe(manager)
             elif choice == '5':
-                print("Exiting application.")
+                print("\nExiting application.")
                 break
             else:
-                print("Invalid choice. Please try again.")
+                print("\nInvalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
